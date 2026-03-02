bad0e6acecef190968c6bba0251edc8701e41375
Albeiro Lozano <Albeiro036@gmail.com>
2026-03-01 23:05:14 -0500

    first config test

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('🚀 ¡IBM i DevPal ha despertado en 2026!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ibmi-devpal" is now active!');
+    // Decoration para colorear 'dcl-s' en verde en archivos .rpgle
+    const greenDecoration = vscode.window.createTextEditorDecorationType({
+        color: 'green',
+        fontWeight: 'bold'
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ibmi-devpal.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ibmi_devpal!');
-	});
+    function updateDecorations(editor: vscode.TextEditor) {
+        if (!editor || editor.document.languageId !== 'rpgle') return;
+        const regEx = /\bdcl-s\b/gi;
+        const text = editor.document.getText();
+        const decorations: vscode.DecorationOptions[] = [];
+        let match;
+        while ((match = regEx.exec(text))) {
+            const startPos = editor.document.positionAt(match.index);
+            const endPos = editor.document.positionAt(match.index + match[0].length);
+            decorations.push({ range: new vscode.Range(startPos, endPos) });
+        }
+        editor.setDecorations(greenDecoration, decorations);
+    }
 
-	context.subscriptions.push(disposable);
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) updateDecorations(editor);
+    }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && event.document === editor.document) {
+            updateDecorations(editor);
+        }
+    }, null, context.subscriptions);
+
+    if (vscode.window.activeTextEditor) {
+        updateDecorations(vscode.window.activeTextEditor);
+    }
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
